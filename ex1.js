27b1cd7c38a1465505961ee8598538d4d45e6472
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-12-02 00:30:26 -0300

    Finalizando o exercicio

--- ex1.js
+++ ex1.js
@@ -1,6 +1,11 @@
 let meuSpan = document.getElementById("meuSpan")
 let modal = document.getElementById("minhaModal")
+var span = document.getElementsByClassName("close")[0];
 
 function AbrirModal(){
     modal.style.display = "block";
 }
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
--- modal.js
+++ modal.js
@@ -3,13 +3,26 @@ function EnviarNota(){
 
     if(inputModal.value < 0 || inputModal.value > 10){
         const node = document.createElement("h5");
-        const textnode = document.createTextNode("Você errou sua nota");
+        const textnode = document.createTextNode("Você errou sua nota, digite novame");
         node.appendChild(textnode);
         let modalContent = document.getElementById("modalContent");
         modalContent.appendChild(node);
         inputModal.value = ''
+        setTimeout(() => {
+            node.style.display = "none"
+        },3000)
         inputModal.focus();
     }else{
-        
+        let modal = document.getElementById("minhaModal")
+        modal.style.display = "none"
+        const node = document.createElement("h3");
+        const textnode = document.createTextNode("Parabéns Você acertou sua nota");
+        node.appendChild(textnode);
+        document.body.appendChild(node);
+        inputModal.value = ''
+        setTimeout(() => {
+            node.style.display = "none"
+        },3000)
     }
 }
+
